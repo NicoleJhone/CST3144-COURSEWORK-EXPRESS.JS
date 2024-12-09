25c230159c6aa3ded44322f72398ebac75ce106b
NicoleJhone <105268456+NicoleJhone@users.noreply.github.com>
2024-12-10 01:49:59 +0400

    added static middleware

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(function (req, res, next) {
   next(); // this should stop the browser from hanging
 });
 
+
+// Sets up the path where the static files are
+var imagePath = path.resolve(__dirname, "images");
+app.use(express.static(imagePath));
+app.use(function (request, response) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("Looks like you didn't find a static file.");
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Express.js server running at localhost:3000");
