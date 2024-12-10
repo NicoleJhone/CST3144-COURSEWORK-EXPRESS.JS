c23173556c1006710dc4b7a46580b8c696d14b2d
NicoleJhone <105268456+NicoleJhone@users.noreply.github.com>
2024-12-10 15:41:04 +0400

    fix

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(function (req, res, next) {
 
 
 app.use(express.json());
-
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
